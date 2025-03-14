221aa93a99c56ad958868b1ae681d38c7b17e68a
nicolas20055 <ortiznicolas656@gmail.com>
2025-03-13 21:15:36 -0500

    casi listo

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -2,24 +2,19 @@ import { FaFacebook, FaInstagram, FaWhatsapp } from "react-icons/fa";
 
 export const Footer = () => {
     return (
-        <>
-            <footer className="w-full h-auto flex flex-col items-center p-4 bg-gray-900 text-white mt-auto">
-                <p>Contactame en:</p>
-                <div className="flex flex-row gap-4 sm:gap-2">
-                
-                    <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
-                        <FaFacebook className="text-xl sm:text-lg hover:text-blue-500 transition" />
-                    </a>
-                    
-                    <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
-                        <FaInstagram className="text-xl sm:text-lg hover:text-pink-500 transition" />
-                    </a>
-                   
-                    <a href="https://api.whatsapp.com/send?phone=573234879314" target="_blank" rel="noopener noreferrer">
-                        <FaWhatsapp className="text-xl sm:text-lg hover:text-green-500 transition" />
-                    </a>
-                </div>
-            </footer>
-        </>
+        <footer className="w-full h-auto flex flex-col items-center p-4 bg-gray-900 text-white mt-auto absolute bottom-0 left-0">
+            <p>Contactame en:</p>
+            <div className="flex flex-row gap-4 sm:gap-2">
+                <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                    <FaFacebook className="text-xl sm:text-lg hover:text-blue-500 transition" />
+                </a>
+                <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+                    <FaInstagram className="text-xl sm:text-lg hover:text-pink-500 transition" />
+                </a>
+                <a href="https://api.whatsapp.com/send?phone=573234879314" target="_blank" rel="noopener noreferrer">
+                    <FaWhatsapp className="text-xl sm:text-lg hover:text-green-500 transition" />
+                </a>
+            </div>
+        </footer>
     );
 };
