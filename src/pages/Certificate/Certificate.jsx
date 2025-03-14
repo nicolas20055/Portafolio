0eb3b9037a7aef8836ca6c89485ce62ef3693362
nicolas20055 <ortiznicolas656@gmail.com>
2025-03-13 21:36:31 -0500

    casi listo

--- src/pages/Certificate/Certificate.jsx
+++ src/pages/Certificate/Certificate.jsx
@@ -1,21 +1,11 @@
-import React from 'react'
+import React from 'react';
 
 export const Certificate = ({ url, titleCertificate }) => {
   return (
-    
-      
-        <a
-          href={url}
-          target="_blank"
-          rel="noopener noreferrer"
-          id="certificatees"
-          className="text-center justify-center  p-5 gap-5 justify-items-center bg-custom-50 max-w-[300px] h-[130px] flex flex-colunderline items-center  rounded-md shadow-md hover:shadow-lg transition duration-300"
-        >
-          <h4>{titleCertificate}</h4>
-        </a>
-      
-    
-  )
-}
+    <a href={url} target="_blank" rel="noopener noreferrer" className="block p-4 bg-white rounded-md shadow-md hover:shadow-lg transition duration-300">
+      <h3 className="text-lg font-semibold text-gray-800">{titleCertificate}</h3>
+    </a>
+  );
+};
 
-export default Certificate
+export default Certificate;
